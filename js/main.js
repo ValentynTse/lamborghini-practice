aafdc5b5868deeea6c8c2e7b65256eb9224d8992
Valentyn <124665175+ValentynTse@users.noreply.github.com>
2023-03-06 14:01:18 +0200

    adds responsive design

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', ()=>{
+    menuMobile.classList.toggle('menu--open');
+});
+
+
+
 const swiper = new Swiper('.swiper', {
     loop: true,
-    slidesPerView: 2,
-    spaceBetween: 20,
+    
     navigation: {
         nextEl: '.swiper-button-right',
         prevEl: '.swiper-button-left',
     },
-
-
-});
-
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+breakpoints:{
+    0:{
+        slidesPerView:1,
+    },
+    860:{
+        slidesPerView: 2,
+        spaceBetween: 20,
+    }
 }
 
-window.initMap = initMap;
+});
